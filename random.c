03594159fd68b471d9267785eb93c14d9c424b83
Terrell Heyward <67223055+tlheyward@users.noreply.github.com>
2024-01-18 22:21:08 -0500

    Created randchar() function

--- random.c
+++ random.c
@@ -1,1 +1,20 @@
-// Hello World!
+/*  
+    CIS 3207 Section 02
+    Terrell Heyward
+    Jan 19 2024
+*/
+
+//  Return random character
+char randchar() {
+
+    /*  
+        Set character to random number between 65 and 90, the ASCII range for lowercase letters a-z
+        using following equation :
+    
+        rand() % (MAX + 1 - MIN) + MIN
+    */
+
+    char c = rand() % 26 + 65;
+
+    return c;
+}
